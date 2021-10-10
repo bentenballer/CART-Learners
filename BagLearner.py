74654a6f9ede0bba7778e61962183ef05bfa038c
Benny <bennychan0913@gmail.com>
2021-10-10 01:58:06 -0400

    Update BagLearner.py

--- BagLearner.py
+++ BagLearner.py
@@ -1,27 +1,3 @@
-""""""
-"""  		  	   		   	 		  		  		    	 		 		   		 		  
-A simple wrapper for linear regression.  (c) 2015 Tucker Balch  		  	   		   	 		  		  		    	 		 		   		 		  
-
-Copyright 2018, Georgia Institute of Technology (Georgia Tech)  		  	   		   	 		  		  		    	 		 		   		 		  
-Atlanta, Georgia 30332  		  	   		   	 		  		  		    	 		 		   		 		  
-All Rights Reserved  		  	   		   	 		  		  		    	 		 		   		 		  
-
-Template code for CS 4646/7646  		  	   		   	 		  		  		    	 		 		   		 		  
-
-Georgia Tech asserts copyright ownership of this template and all derivative  		  	   		   	 		  		  		    	 		 		   		 		  
-works, including solutions to the projects assigned in this course. Students  		  	   		   	 		  		  		    	 		 		   		 		  
-and other users of this template code are advised not to share it with others  		  	   		   	 		  		  		    	 		 		   		 		  
-or to make it available on publicly viewable websites including repositories  		  	   		   	 		  		  		    	 		 		   		 		  
-such as github and gitlab.  This copyright statement should not be removed  		  	   		   	 		  		  		    	 		 		   		 		  
-or edited.  		  	   		   	 		  		  		    	 		 		   		 		  
-
-We do grant permission to share solutions privately with non-students such  		  	   		   	 		  		  		    	 		 		   		 		  
-as potential employers. However, sharing with other current or future  		  	   		   	 		  		  		    	 		 		   		 		  
-students of CS 7646 is prohibited and subject to being investigated as a  		  	   		   	 		  		  		    	 		 		   		 		  
-GT honor code violation.  		  	   		   	 		  		  		    	 		 		   		 		  
-
------do not edit anything above this line---  		  	   		   	 		  		  		    	 		 		   		 		  
-"""
 import numpy as np
 from numpy import random
 
@@ -40,12 +16,6 @@ class BagLearner(object):
             self.learners.append(learner(**kwargs))
         self.bags = bags
 
-    def author(self):
-        """
-        :return: The GT username of the student
-        :rtype: str
-        """
-        return "cchan313"  # replace tb34 with your Georgia Tech username
 
     def bootstrap(self, data):
         bag = np.zeros(data.shape[1])
